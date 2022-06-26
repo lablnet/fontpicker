23a99e076a2f6128e094172aac2b3abaeae00225
Muhammad Umer Farooq <umer@lablnet.com>
2022-06-26 17:43:29 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import json
+from script.fonts import prepare_url, get_fonts_from_gapi, parse_fonts, create_json
+import json
+
+url = "https://www.googleapis.com/webfonts/v1/webfonts?key=:KEY"
+
+if __name__ == "__main__":
+    url = prepare_url(url)
+    fonts = get_fonts_from_gapi(url)
+    if fonts:
+        fonts = parse_fonts(fonts)
+        create_json(fonts)
+        # print(json.dumps(fonts, indent=4))
+    else:
+        print("Error")
---
+++ script/fonts.py
@@ -0,0 +1,87 @@
+import requests
+import json
+from typing import Union
+import os
+from dotenv import load_dotenv
+
+# Load .env file
+load_dotenv()
+
+
+def prepare_url(url: str) -> str:
+    """ Prepare url for Google Fonts API.
+    :param url: Google Fonts API url.
+    :return: Prepared url.
+    :throw: None.
+    :rtype: str.
+    :Example:
+    >>> prepare_url(url)
+    """
+    return url.replace(':KEY', os.getenv('GOOGLE_API_KEY'))
+
+
+def get_fonts_from_gapi(url: str) -> Union[dict, None]:
+    """ Get fonts from Google Fonts API.
+    :param url: Google Fonts API url.
+    :return: Dictionary with fonts if successful, None otherwise.
+    :throw: None.
+    :rtype: dict or None.
+    :Example:
+    >>> get_fonts(url)
+    """
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = json.loads(response.text)
+            return data["items"]
+        else:
+            return None
+    except Exception as e:
+        return None
+
+
+def parse_fonts(fonts: dict) -> dict:
+    """ Parse fonts from Google Fonts API.
+    :param fonts: Dictionary with fonts.
+    :return: Dictionary with parsed fonts.
+    :throw: None.
+    :rtype: dict.
+    :Example:
+    >>> parse_fonts(fonts)
+    """
+    data = {
+        "languages": [],
+        "fonts": [],
+        "categories": []
+    }
+    for font in fonts:
+        languages = font['subsets']
+        category = font['category']
+        for lang in languages:
+            if lang not in data['languages']:
+                data['languages'].append(lang)
+        if category not in data['categories']:
+            data['categories'].append(category)
+        data['fonts'].append(
+            {
+                # remove space with +.
+                'family': font['family'].replace(' ', '+'),
+                'variants': font['variants'],
+                'files': font['files'],
+                'lastModified': font['lastModified']
+            }
+        )
+    return data
+
+
+def create_json(data: dict) -> None:
+    """ Create json file with fonts.
+    :param data: Dictionary with fonts.
+    :return: None.
+    :throw: None.
+    :rtype: None.
+    :Example:
+    >>> create_json(data)
+    """
+    with open('fonts.json', 'w') as f:
+        json.dump(data, f, indent=4)
